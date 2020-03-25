576ced15b3f229ef517bf159564ba0f54cc8cb09
rohanharikr <rohan@kaarva.com>
2020-03-26 01:32:20 +0530

    some ui fixes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let counter = 0; 
 let inputlength = 0;
-let timeCreated = new Date().toLocaleTimeString();
-let title;
+let timeCreated;
+let notetitle;
 
 $( "#tostore" ).keyup(function() {
   if (event.keyCode === 13) {
@@ -10,41 +10,50 @@ $( "#tostore" ).keyup(function() {
   }
 
   inputlength = $('#tostore').val().length;
-  console.log(inputlength);
-
   $("#charlength").html(inputlength);
 
 });
 
 $( "#branding" ).keyup(function() {
-	console.log("hmm")
-	title = $('#branding').html();
-
+	title = $('#branding').find('span').html();
+	$('title').html(title);
 	localStorage.setItem("title", title);
 
+	if($('title').find('span').html() < 1){
+		$('title').find('span').html("add a note.")
+	}
 });
 
 (function getinputvalue(){
+
+	// $('#branding').find('br').remove();
+
 	inputlength = $('#tostore').val().length;
 	$("#charlength").html(inputlength);
 
 	if(localStorage.getItem("title") !== null){
-		$('#branding').html(localStorage.getItem("title"));
+		$('#branding').find('span').html(localStorage.getItem("title"));
+		$('title').html(localStorage.getItem("title"));
 	}
 
 })();
 
 function storeLocal(){
 	const input = $('#tostore').val();
+	timeCreated = Date().toLocaleString() ;
 
 	if (input !== ""){
 		counter++;
-		console.log(input);
-		$("#list").append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}"><label for="checkbox${counter}">${input}</label></div>`)
+		$("#list").append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}"><label for="checkbox${counter}">${input}</div>`)
 		$('#tostore').val("");
 	} else {
-		throw new Error("input field empty")
+		throw new Error("input field empty");
 	}
+}
+
+function deleteItem() {
+	console.log("dingo")
+    $(this).remove();
 }
 
 function deleteList(){
