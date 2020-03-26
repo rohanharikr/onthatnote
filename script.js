fede628f6614b63cef7c4dd1fc221584aee37125
rohanharikr <rohan@kaarva.com>
2020-03-27 05:12:46 +0530

    code refactoring

--- script.js
+++ script.js
@@ -1,154 +1,138 @@
-let counter = 0;
-let inputlength = 0;
-let timeCreated;
-let notetitle;
-let createList = 0;
-let completed = 0;
+let counter = 0,
+    inputlength = 0,
+    timeCreated,
+    title,
+    checked,
+    createList = 0,
+    completed = 0;
 
-function changemode(){
-	$("#sun").toggle();
-	$("#moon").toggle();
+
+function changemode() {
+  $('#sun').toggle();
+  $('#moon').toggle();
 }
 
-$("#tostore").keyup(function (event) {
-	if (event.keyCode === 13) {
-		event.preventDefault();
-		storeLocal();
-	}
 
-	 if (inputlength >= 2) {
-		$("#helper").show();
-	} else {
-		$("#helper").hide();
-	}
-	
-	inputlength = $('#tostore').val().length;
-	$("#charlength").html(inputlength);
+$('#tostore').keyup(function(event) {
+  inputlength = $('#tostore').val().length;
 
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    storeLocal();
+  }
+
+  if (inputlength >= 2) {
+    $('#helper').show();
+  } else {
+    $('#helper').hide();
+  }
+
+  $('#charlength').html(inputlength);
 });
 
-$('#tostore').on('kedown', function() {
-   //code to be executed
- }).on('keydown', function(e) {
-   if (inputlength >= 2) {
-		$("#helper").show();
-	}
- });
-
-$("#branding").keyup(function () {
-	title = $('#branding').find('span').html();
-	$('title').html(title);
-	localStorage.setItem("title", title);
-
-	if ($('title').find('span').html() < 1) {
-		$('title').find('span').html("add a note.")
-	}
-});
-
-$("#tostore").click(function () {
-	
-});
-
-$("#branding").click(function () {
-	let title = $('#branding').find('span').html();
-
-	if (title == "give me a name") {
-		$('#branding').find('span').html('');
-	}
-});
-
-(function getinputvalue() {
-
-	// $('#branding').find('br').remove();
-
-	inputlength = $('#tostore').val().length;
-	$("#charlength").html(inputlength);
-
-	if (localStorage.getItem("title") !== null) {
-		$('#branding').find('span').html(localStorage.getItem("title"));
-		$('title').html(localStorage.getItem("title"));
-	}
-
-})();
 
 function storeLocal() {
-	$("#helper").hide();
-	const input = $('#tostore').val();
-	timeCreated = new Date().toLocaleTimeString();
+  checked = (createList - completed);
+  const input = $('#tostore').val();
+  timeCreated = new Date().toLocaleTimeString();
 
-	$("#completed, #list").show();
+  $('#helper').hide();
+  $('#completed, #list').show();
 
-	if (input !== "") {
-		counter++;
-		$("#list").append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}"><label for="checkbox${counter}">${input}</label><span class="timecreated">${timeCreated}</span><span id="deleteitem"><i class="fas fa-trash-alt"></i> Delete</span></div>`)
-		$('#tostore').val("");
-	} else {
-		throw new Error("input field empty");
-	}
+  if (input !== '') {
+    counter++;
+    createList++;
+    checked++;
 
-	createList++;
-	$(".listcounter").html(createList);
+    $('#list').append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}"><label for="checkbox${counter}">${input}</label><span class="timecreated">${timeCreated}</span><span id="deleteitem"><i class="fas fa-trash-alt"></i> Delete</span></div>`);
+    $('#tostore').val('');
+  } else {
+    throw new Error('input field empty');
+  }
 
-	let checked = (createList - completed)
-	$(".completed").html(checked);
+  $('.listcounter').html(createList);
+  $('.completed').html(checked);
 }
 
-$(document).on('click', '#deleteitem', function () {
-	// console.log("del")
-	$(this).parent().remove();
 
-	createList--;
-	$(".listcounter").html(createList);
+$('#branding').bind('click keyup', (function() {
+  title = $('#branding').find('span').html();
+  
+  $('title').html(title);
+  localStorage.setItem('title', title);
 
-	let checked = (createList - completed)
-	$(".completed").html(checked);
+  if ( $('title').find('span').html() < 1 ) {
+    $('title').find('span').html('add a note.');
+  }
+
+  if (title == 'give me a name') {
+    $('#branding').find('span').html('');
+  }
+}));
+
+
+$(function getinputvalue() {
+  inputlength = $('#tostore').val().length;
+  
+  $('#charlength').html(inputlength);
+
+  if (localStorage.getItem('title') !== null) {
+    $('#branding').find('span').html(localStorage.getItem('title'));
+    $('title').html(localStorage.getItem('title'));
+  }
 });
+
+$(document).on('click', '#deleteitem', function deleteitem() {
+  let checked = (createList - completed);
+  
+  $(this).parent().remove();
+  createList--;
+  checked--;
+  
+  $('.listcounter').html(createList);  
+  $('.completed').html(checked);
+});
+
 
 function deleteList() {
-	$("#list").html("");
-	localStorage.clear();
+  $('#list').html('');
+  localStorage.clear();
 }
 
 
-$(document).on('mousedown', '#checkbox', function () {
-
-	if (!$(this).is(':checked')) {
-
-		let complete = $(this).find('label').html();
-		completed++;
-
-		var completedTask = $(this).parent().find('label').text();
-
-		let checked = (createList - completed)
-
-		$(".completed").html(checked);
-		$(".checked").html(completed);
-		$("#completed").append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}" checked><label for="checkbox${counter}">${completedTask}</label></div>`)
-		// $(this).fadeOut(600, function() {$(this).parent().remove(); });
-		$(this).parent().remove();
-	}
-});
-
-
-clearlocalstorage = () => {
-	localStorage.clear();
-	location.reload();
-};
-
-window.addEventListener('click', function(e){   
-  if (!document.getElementById('tostore').contains(e.target) && $('#tostore').val() !== ""){
-  	storeLocal();
+$(document).on('mousedown', '#checkbox', function() {
+  if ( !$(this).is(":checked") ) {
+    let completedTask = $(this).parent().find('label').text();
+    let checked = (createList - completed);
+    completed++;
+    checked--;
+    $('.completed').html(checked);
+    $('.checked').html(completed);
+    $('#completed').append(`<div class="list"><input type="checkbox" id="checkbox" name="checkbox${counter}" checked><label for="checkbox${counter}">${completedTask}</label></div>`);
+    $(this).parent().remove();
   }
 });
 
 
-$("#sort").click(function () {
-	$('#list .list').sort(function(a, b) {
-	    return a.id < b.id ? -1 : 1;
-	}).appendTo('#list');
-
-	$('#completed .list').sort(function(a, b) {
-	    return a.id < b.id ? -1 : 1;
-	}).appendTo('#completed');
+$('#dellocalstorage').click(function clearlocalstorage() {
+  localStorage.clear();
+  location.reload();
 });
 
+
+window.addEventListener('click', function(e) {
+  if (!document.getElementById('tostore').contains(e.target) && $('#tostore').val() !== "") {
+    storeLocal();
+  }
+});
+
+
+$('#sort').click(function() {
+  $('#list .list').sort(function(a, b) {
+  return a.id < b.id ? -1 : 1;
+  }).appendTo('#list');
+
+  $('#completed .list').sort(function(a, b) {
+  return a.id < b.id ? -1 : 1;
+  }).appendTo('#completed');
+});
